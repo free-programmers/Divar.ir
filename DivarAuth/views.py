c9d9a796af1f4368dd45a4074bc30e4399ce5e69
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-05-28 13:03:31 +0330

    add constans

--- DivarAuth/views.py
+++ DivarAuth/views.py
@@ -1,12 +1,11 @@
 from DivarAuth import auth
-from flask_jwt_extended import jwt_required
 from DivarCore.utils import json_only, ArgParser
-from flask import request
+
 
 
 loginArg = ArgParser()
-loginArg.add_rules(Fname="username", Ferror="username required")
-
+loginArg.add_rules(Fname="username", Ferror="username is required")
+loginArg.add_rules(Fname="password", Ferror="password is required")
 @auth.route("/login/", methods=["POST"])
 @json_only
 @loginArg.verify
--- DivarCore/extenstion.py
+++ DivarCore/extenstion.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_session import Session
 from flask_jwt_extended import JWTManager
+# from flask_wtf.csrf import CSRFProtect
+
 
 session = Session()
 migrate = Migrate()
 db = SQLAlchemy()
 jwtMNG = JWTManager()
-
+# csrf = CSRFProtect()
