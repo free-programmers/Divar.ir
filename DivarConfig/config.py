2a53308a742fc7336bf6abdf78f8cbdae5623cfc
alisharify <alyofficial@gmail.com>
2023-08-04 19:47:14 +0330

    point config to .env file

--- DivarConfig/config.py
+++ DivarConfig/config.py
@@ -1,33 +1,36 @@
 import os
-import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-print(os.environ)
 
 BASEDIR = Path(__file__).parent.parent
+APP_DEBUG = True if os.environ.get("APP_DEBUG") == "True" else False
+
 
 # sms api key
-API_KEY = os.environ.get("API_KEY")
+API_KEY = os.environ.get("API_KEY", "NULL")
 
 
-NAME_DB = "divar"
-USERNAME_DB = "divar" # local db
-PASSWORD_DB = "123654" # test password for localhost only
-HOST_DB = "localhost"
-PORT_DB = 3306
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "DIVAR")
+DATABASE_USER = os.environ.get("DATABASE_USER", "root")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "")
+DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.environ.get("DATABASE_PORT", "3306")
 
 
 APP_RUNNER_CONFIG ={
-    "port": 8080,
-    "debug": True,
-    "host": "0.0.0.0"
+    "port": os.environ.get("APP_PORT", 5000),
+    "debug": APP_DEBUG,
+    "host": os.environ.get("APP_HOST", "localhost")
 }
 
 
 class BaseConfig:
     """ base class for flask app config """
+
+    FLASK_DEBUG = 1 if APP_DEBUG else 0
+
     SECRET_KEY = "app and app"
     WTF_CSRF_SECRET_KEY = "app and app"
 
@@ -36,28 +39,21 @@ class BaseConfig:
 
     # sqlalchemy config
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME_DB}:{PASSWORD_DB}@{HOST_DB}:{PORT_DB}/{NAME_DB}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
     # redis
-    REDIS_URL = "redis://localhost:6379"
+    REDIS_PRODUCTION_URI = os.environ.get("REDIS_PRODUCTION_URI", "redis://localhost:6379")
+    REDIS_LOCAL_URI = os.environ.get("REDIS_LOCAL_URI", "redis://localhost:6379")
 
-    # Session config
-    # SESSION_PERMANENT = False
-    # SESSION_TYPE = "filesystem"
-    # SESSION_COOKIE_SECURE = True
-    # SESSION_COOKIE_SECURE = True
-    # SESSION_COOKIE_HTTPONLY = True
-    # SESSION_COOKIE_SAMESITE = 'Lax'
-    # SESSION_COOKIE_NAME = 'DivarSessionApp'
-    # PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=16)
+    REDIS_URI = REDIS_PRODUCTION_URI
 
     # mail config
-    MAIL_SERVER = ""
-    MAIL_PORT = 587
-    MAIL_USERNAME = ""
-    MAIL_PASSWORD = ""
-    MAIL_USE_TLS = True
-    MAIL_DEBUG = False
+    MAIL_SERVER = os.environ.get("MAIL_SERVER", "")
+    MAIL_PORT = os.environ.get("MAIL_PORT", 587)
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "")
+    MAIL_USE_TLS = True if os.environ.get("MAIL_USE_TLS") == "True" else False
+    MAIL_DEBUG = os.environ.get("MAIL_DEBUG", "")
 
     # recaptcha v3
     RECAPTCHA_PUBLIC_KEY = os.getenv("RECAPTCHA_PUBLIC_KEY")
